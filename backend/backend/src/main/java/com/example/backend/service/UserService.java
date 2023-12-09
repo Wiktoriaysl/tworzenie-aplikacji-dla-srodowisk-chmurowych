ffbb7000c4eb63a1f9f4ba3f33989f97f9d64cad
wika <szendzielorz.wik@protonmail.com>
2023-12-09 19:01:00 +0100

    add base files

---
+++ backend/backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.backend.service;
+
+import com.example.backend.persistence.UserEntity;
+import com.example.backend.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+    public UserEntity getUserById(Integer id){
+        return userRepository.getUserEntityById(id);
+    }
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
