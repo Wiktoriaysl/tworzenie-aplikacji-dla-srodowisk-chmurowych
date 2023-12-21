79a43c1f682fe54c9a1df0778a82cfc4008417b6
wika <szendzielorz.wik@protonmail.com>
2023-12-21 21:32:57 +0100

    .

--- backend/backend/src/main/java/com/example/backend/persistence/UserEntity.java
+++ backend/backend/src/main/java/com/example/backend/persistence/UserEntity.java
@@ -1,6 +1,8 @@
 package com.example.backend.persistence;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,10 +11,21 @@ import jakarta.persistence.Table;
 public class UserEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
     private String status;
+    
+    public UserEntity(String name, String email, String status) {
+        this.name = name;
+        this.email = email;
+        this.status = status;
+    }
+
+    public UserEntity() {
+
+    }
 
     public Integer getId() {
         return id;
@@ -26,5 +39,14 @@ public class UserEntity {
 
     public String getStatus() {
         return status;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setStatus(String status){
+        this.status = status;
     }
 }
